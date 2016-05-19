7b5711ee6396fc82df08b653b53e25bb5e4a93bd
Buddhike de Silva <buddhike.desilva@geeksdiary.com>
2016-05-19 18:54:07 +1000

    Authorize idiom echos the input.

--- test/authorize.js
+++ test/authorize.js
@@ -8,4 +8,9 @@ describe('authorize', () => {
   it('should not throw if value is truthey', () => {
     authorize(true);
   });
+
+  it('should return the same value if it is a truthey', () => {
+    const i = {};
+    authorize(i).should.equal(i);
+  });
 });
